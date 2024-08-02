16e0da7e5882c0049a9fc217d8c832f7d6de2b55
DDDEC1DE <mellinov337@gmail.com>
2024-08-02 18:16:30 -0400

    Progress

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -7,8 +7,8 @@ public class PlayerShooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float bulletSpeed = 20f;
-    public int maxAttempts = 3;
-    private int currentAttempts = 0;
+    public int maxAttempts = 10;
+    public int currentAttempts = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +21,11 @@ public class PlayerShooting : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && currentAttempts < maxAttempts)
         {
+            RotateTowardsMouse();
             Shoot();
         }
+
+       
     }
     void Shoot()
     {
@@ -30,5 +33,20 @@ public class PlayerShooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.velocity = firePoint.forward * bulletSpeed;
+    }
+
+    void RotateTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(ray, out rayLength))
+        {
+            Vector3 pointToLook = ray.GetPoint(rayLength);
+            Vector3 direction = pointToLook - transform.position;
+            direction.y = 0; // „тобы вращение было только по горизонтали
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
     }
 }
